a1f9501fd956a3c1a05d5804b783077718ebaef4
sophiafan <172404042@qq.com>
2019-04-17 18:41:09 +0800

    small changes

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -14,6 +14,6 @@ class Api::V1::EventsController < Api::V1::BaseController
   end
 
    def event_params
-    params.require(:event).permit(:timestamp, :description)
+    params.require(:event).permit(:id, :description, :timestamp, :page, :customer_id, :shop_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :events, only: [:index]
+      resources :events, only: [:index, :create]
     end
   end
 end
