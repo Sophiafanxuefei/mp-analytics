70d0204689037663fe257fe08d8b48c88562094e
sophiafan <172404042@qq.com>
2019-04-18 15:15:27 +0800

    delete ngrok

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,7 +8,11 @@ class Api::V1::EventsController < Api::V1::BaseController
     @event = Event.new
   end
 
-   def create
+  def create
+    #the following 3 lines are for ngrok to work, can delete after no need ngrok
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Headers'] = 'accept, content-type'
     @event = Event.new(event_params)
     @event.save
   end
